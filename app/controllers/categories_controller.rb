743455ba91f04647c710adbad294090a4ae1fddb
Marcus Veloso <desenvolvedoroficial@gmail.com>
2021-02-16 02:02:19 -0300

    add redirect to index in categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -28,8 +28,8 @@ class CategoriesController < ApplicationController
 
     respond_to do |format|
       if @category.save
-        format.html { redirect_to @category, notice: 'Category was successfully created.' }
-        format.json { render :show, status: :created, location: @category }
+        format.html { redirect_to action: "index", notice: 'Category was successfully created.' }
+        format.json { render :index, status: :created, location: @category }
       else
         format.html { render :new }
         format.json { render json: @category.errors, status: :unprocessable_entity }
@@ -42,8 +42,8 @@ class CategoriesController < ApplicationController
   def update
     respond_to do |format|
       if @category.update(category_params)
-        format.html { redirect_to @category, notice: 'Category was successfully updated.' }
-        format.json { render :show, status: :ok, location: @category }
+        format.html { redirect_to action: "index", notice: 'Category was successfully updated.' }
+        format.json { render :index, status: :ok, location: @category }
       else
         format.html { render :edit }
         format.json { render json: @category.errors, status: :unprocessable_entity }
@@ -56,7 +56,7 @@ class CategoriesController < ApplicationController
   def destroy
     @category.destroy
     respond_to do |format|
-      format.html { redirect_to categories_url, notice: 'Category was successfully destroyed.' }
+      format.html { redirect_to action: "index", notice: 'Category was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
